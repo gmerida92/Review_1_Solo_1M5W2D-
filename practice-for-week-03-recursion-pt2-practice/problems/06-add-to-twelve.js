149ec4ab1d3a83d095a5981a78c3046f45709256
George Merida <gmerida92@gmail.com>
2024-02-05 22:05:11 -0800

    Commit 1 Edit Case and Step

--- practice-for-week-03-recursion-pt2-practice/problems/06-add-to-twelve.js
+++ practice-for-week-03-recursion-pt2-practice/problems/06-add-to-twelve.js
@@ -16,16 +16,19 @@ addToTwelve([1]); // false
 function addToTwelve(arr) {
   // Your code here
   // Base Case
-  console.log('HERE1!!', arr)
-  if(!arr || arr.length === 1) return false;
+  if (arr.length < 2) return false
+  // console.log('HERE1!!', arr)
+  // if(!arr || arr.length === 1) return false;
   
-  let pointer = arr.shift()
-  console.log('HERE2!!', pointer)
-  console.log('HERE3!!', pointer + arr[0])
-  if (pointer + arr[0] === 12) return true
+  // let pointer = arr.shift()
+  // console.log('HERE2!!', pointer)
+  // console.log('HERE3!!', pointer + arr[0])
+  // if (pointer + arr[0] === 12) return true
+
+  if(arr[0] + arr[1] === 12) return true
   
   // Recursive Step
-  
+  arr.shift();
   return addToTwelve(arr)
 
 
